36649dad8fbe673216a065e473f8090ab82ea699
Sergey Mikhalchenko <mikhalchenko.serg@gmail.com>
2023-09-11 12:24:40 +0300

    feat: added check functional + data output functionality

--- shipment-form/src/App.jsx
+++ shipment-form/src/App.jsx
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SendingForm from "./SendingForm";
 import PackagesList from "./PackagesList";
 
 function App() {
+  const [packList, setPackList] = useState([]);
+
+  const addUserHandler = (
+    senderName,
+    senderPhone,
+    senderAddress,
+
+    addresseeName,
+    addresseePhone,
+    addresseeAddress,
+
+    length,
+    width,
+    height,
+    weight,
+
+    insurance,
+    typeOfSend
+  ) => {
+    setPackList((prevPackList) => [
+      ...prevPackList,
+      {
+        senderName: senderName,
+        senderPhone: senderPhone,
+        senderAddress: senderAddress,
+
+        addresseeName: addresseeName,
+        addresseePhone: addresseePhone,
+        addresseeAddress: addresseeAddress,
+
+        length: length,
+        width: width,
+        height: height,
+        weight: weight,
+        insurance: insurance,
+        typeOfSend: typeOfSend,
+
+        id: Math.random().toString(),
+      },
+    ]);
+  };
+
   return (
     <>
-      <SendingForm />
-      <PackagesList />
+      <SendingForm onSendingForm={addUserHandler} />
+      <PackagesList packList={packList} />
     </>
   );
 }
--- shipment-form/src/PackagesList.jsx
+++ shipment-form/src/PackagesList.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import "./PackagesList.css";
 
-function PackagesList() {
+function PackagesList({ packList }) {
   return (
     <>
-      <div className="packages-list-wrapper">
-        <div className="package-item-wrapper">
-          <h4>От</h4>
-          <p>Имя:</p>
-          <p>Телефон</p>
-          <p>Адрес</p>
+      {packList.map((pack) => (
+        <div className="packages-list-wrapper" key={pack.id}>
+          <div className="package-item-wrapper">
+            <h4>От</h4>
+            <p>Имя: {pack.senderName}</p>
+            <p>Тел: {pack.senderPhone}</p>
+            <p>Адр: {pack.senderAddress}</p>
+          </div>
+          <div className="package-item-wrapper">
+            <h4>Кому</h4>
+            <p>Имя: {pack.addresseeName}</p>
+            <p>Тел: {pack.addresseePhone}</p>
+            <p>Адр: {pack.addresseeAddress}</p>
+          </div>
+          <div className="package-item-wrapper">
+            <p>Страховка: {pack.insurance ? "есть" : "нет"}</p>
+            <p>Отправление: {pack.typeOfSend}</p>
+            <p>Вес (кг): {pack.weight}</p>
+            <p>
+              Д х Ш х В (см): {pack.length} x {pack.width} x {pack.height}
+            </p>
+          </div>
         </div>
-        <div className="package-item-wrapper">
-          <h4>Кому</h4>
-          <p>Имя</p>
-          <p>Телефон</p>
-          <p>Адрес</p>
-        </div>
-        <div className="package-item-wrapper">
-          <p>Страховка</p>
-          <p>Отправление</p>
-          <p>Вес (кг)</p>
-          <p>Д х Ш х В (см)</p>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
